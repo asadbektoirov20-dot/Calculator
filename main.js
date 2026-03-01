da7b6c8b94335b7b0d0033d324928b890606fda2
asadbektoirov20-dot <asadbektoirov20@gmail.com>
2026-03-01 16:34:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+let inp = document.getElementById('ekran')
+
+function qush(ll) {
+    inp.value += ll
+
+}
+function uchir() {
+    inp.value = ""
+}
+
+function qaytar() {
+    inp.value = inp.value.slice(0, -1)
+}
+
+function teng() {
+    inp.value = eval(inp.value)
+}
+
+
+
